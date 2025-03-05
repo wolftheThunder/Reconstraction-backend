5ea1e9ca5df04dc3c03c0a25fcca8bd9a70f3570
shabbir ali <shabbiralikhan326@gmail.com>
2025-03-06 03:11:06 +0500

    server

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const servicesRoutes = require("./routes/serviceRoutes");
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
