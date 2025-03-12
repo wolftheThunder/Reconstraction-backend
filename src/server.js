81b9ea252a34a2e975fa67ee6b8984e322de7a49
qasimfida <qasimfida0054@gmail.com>
2025-03-12 08:12:01 +0500

    database

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require('path');
-const fs = require("fs");
-const https = require("https");
 require("dotenv").config();
 
 const contactMessageRoutes = require("./routes/contactMessageRoutes");
@@ -16,14 +14,6 @@ const servicesRoutes = require("./routes/serviceRoutes");
 
 const app = express();
 
-// Load SSL Certificates
-const options = {
-    key: fs.readFileSync(path.resolve(__dirname, '../ssl/key.pem')),
-    cert: fs.readFileSync(path.resolve(__dirname, '../ssl/cert.pem'))
-};
-
-
-// Enable CORS
 app.use(cors({
     origin: '*',
     credentials: true
@@ -35,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
 
-// API Routes
 app.use("/api", contactMessageRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/projects", projectRoutes);
@@ -43,7 +32,6 @@ app.use("/api/projects", reviewRoutes);
 app.use("/api/unapproved-reviews", unapprovedReviewRoutes);
 app.use("/api/services", servicesRoutes);
 
-// Error Handling
 app.use((err, req, res, next) => {
     console.error('Error:', err.stack);
     res.status(err.status || 500).json({
@@ -55,7 +43,7 @@ app.use((req, res) => {
     res.status(404).json({ error: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 443; // Use 443 for HTTPS
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
@@ -65,14 +53,13 @@ const startServer = async () => {
         await sequelize.sync({ alter: true });
         console.log("Database synchronized");
 
-        // Start HTTPS server
-        https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
-            console.log(`🚀 Secure server running at https://your-server-ip:${PORT}`);
-            console.log(`📂 Upload path: ${path.join(__dirname, '../uploads')}`);
+app.listen(3000, '0.0.0.0', () => {
+            console.log(`Server running on port ${PORT}`);
+            console.log(`Upload path: ${path.join(__dirname, '../uploads')}`);
         });
     } catch (error) {
-        console.error("❌ Server startup failed:");
-        console.error("Error name:", error.name);
+        console.error("Server startup failed:");
+        console.error("ErPORT ror name:", error.name);
         console.error("Error message:", error.message);
         
         if (error.name === 'SequelizeConnectionError') {
